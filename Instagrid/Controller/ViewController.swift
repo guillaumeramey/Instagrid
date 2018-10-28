468f59c2244bf0e8e28b97ae8b33dcd429ce613a
Guillaume Ramey <guillaume.ramey@gmail.com>
2018-10-28 21:45:52 +0200

    interface development

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonLayout1: UIButton!
+    @IBOutlet weak var buttonLayout2: UIButton!
+    @IBOutlet weak var buttonLayout3: UIButton!
+    @IBOutlet weak var gridRow1: UIStackView!
+    @IBOutlet weak var gridRow2: UIStackView!
+    @IBOutlet weak var gridRow3: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        layout = .layout2
     }
-
     
+    enum Layout {
+        case layout1, layout2, layout3
+    }
+    
+    var layout: Layout = .layout2 {
+        didSet {
+            setLayout(layout)
+        }
+    }
+    
+    @IBAction func buttonLayout1(_ sender: Any) {
+        layout = .layout1
+    }
+    
+    @IBAction func buttonLayout2(_ sender: Any) {
+        layout = .layout2
+    }
+    
+    @IBAction func buttonLayout3(_ sender: Any) {
+        layout = .layout3
+    }
+    
+    func setLayout(_ layout: Layout) {
+        switch layout {
+        case .layout1:
+            buttonLayout1.setImage(UIImage(named: "Button Selected.png"), for: .normal)
+            buttonLayout2.setImage(UIImage(named: ""), for: .normal)
+            buttonLayout3.setImage(UIImage(named: "B"), for: .normal)
+            gridRow1.isHidden = true
+            gridRow2.isHidden = false
+            gridRow3.isHidden = false
+        case .layout2:
+            buttonLayout1.setImage(UIImage(named: ""), for: .normal)
+            buttonLayout2.setImage(UIImage(named: "Button Selected.png"), for: .normal)
+            buttonLayout3.setImage(UIImage(named: ""), for: .normal)
+            gridRow1.isHidden = false
+            gridRow2.isHidden = false
+            gridRow3.isHidden = true
+        case .layout3:
+            buttonLayout1.setImage(UIImage(named: ""), for: .normal)
+            buttonLayout2.setImage(UIImage(named: ""), for: .normal)
+            buttonLayout3.setImage(UIImage(named: "Button Selected.png"), for: .normal)
+            gridRow1.isHidden = false
+            gridRow2.isHidden = true
+            gridRow3.isHidden = false
+        }
+    }
+ 
 }
 
