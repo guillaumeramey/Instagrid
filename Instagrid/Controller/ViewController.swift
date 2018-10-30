29ca2e22a1bf6fad61c47252f6163d83094efe76
Guillaume Ramey <guillaume.ramey@gmail.com>
2018-10-30 17:28:16 +0200

    added resources use only 4 images instead of 5

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var gridRow1: UIStackView!
-    @IBOutlet weak var gridRow2: UIStackView!
-    @IBOutlet weak var gridRow3: UIStackView!
+    @IBOutlet weak var gridView: UIView!
     @IBOutlet weak var buttonLayout1: UIButton!
     @IBOutlet weak var buttonLayout2: UIButton!
     @IBOutlet weak var buttonLayout3: UIButton!
@@ -20,8 +18,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var buttonImage2: UIButton!
     @IBOutlet weak var buttonImage3: UIButton!
     @IBOutlet weak var buttonImage4: UIButton!
-    @IBOutlet weak var buttonImage5: UIButton!
 
+    var imagePicker = UIImagePickerController()
+    var buttonSender: UIButton!
+    
+    // Layout buttons selection
     @IBAction func tapButtonLayout1(_ sender: Any) {
         setLayout(1)
     }
@@ -31,6 +32,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func tapButtonLayout3(_ sender: Any) {
         setLayout(3)
     }
+    
+    // Image buttons selection
     @IBAction func tapButtonImage1(_ sender: UIButton) {
         buttonSender = buttonImage1
         self.present(imagePicker, animated: true)
@@ -47,13 +50,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         buttonSender = buttonImage4
         self.present(imagePicker, animated: true)
     }
-    @IBAction func tapButtonImage5(_ sender: UIButton) {
-        buttonSender = buttonImage5
-        self.present(imagePicker, animated: true)
-    }
-    
-    var imagePicker = UIImagePickerController()
-    var buttonSender: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,63 +57,81 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
         imagePicker.sourceType = .savedPhotosAlbum
         imagePicker.allowsEditing = false
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragGridView(_:)))
+        gridView.addGestureRecognizer(panGestureRecognizer)
     }
     
+    // allows the user to select an image
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
         buttonSender.setImage(pickedImage, for: .normal)
         buttonSender.imageView?.contentMode = UIView.ContentMode.scaleAspectFill
-        buttonSender.backgroundColor = #colorLiteral(red: 0.06274509804, green: 0.4, blue: 0.5960784314, alpha: 1)
         dismiss(animated: true)
     }
     
+    // canceling the image picker
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true)
     }
     
+    // sets the grid layout
     func setLayout(_ layout: Int) {
         switch layout {
         case 1:
             selectButtonLayout(buttonLayout1)
-            showRow(gridRow2)
-            showRow(gridRow3)
-            hideRow(gridRow1)
+            hideButton(buttonImage2)
+            showButton(buttonImage4)
         case 2:
             selectButtonLayout(buttonLayout2)
-            showRow(gridRow1)
-            showRow(gridRow2)
-            hideRow(gridRow3)
+            showButton(buttonImage2)
+            hideButton(buttonImage4)
         case 3:
             selectButtonLayout(buttonLayout3)
-            showRow(gridRow1)
-            showRow(gridRow3)
-            hideRow(gridRow2)
+            showButton(buttonImage2)
+            showButton(buttonImage4)
         default:
             break
         }
     }
     
+    // selects the current layout button
     func selectButtonLayout(_ buttonLayout: UIButton) {
         buttonLayout1.setImage(UIImage(named: ""), for: .normal)
         buttonLayout2.setImage(UIImage(named: ""), for: .normal)
         buttonLayout3.setImage(UIImage(named: ""), for: .normal)
         buttonLayout.setImage(UIImage(named: "Button Selected.png"), for: .normal)
     }
+
+    // shows the button image
+    func showButton(_ button: UIButton) {
+        UIView.transition(with: button, duration: 0.4, options: .transitionCrossDissolve, animations: {
+            button.isHidden = false
+        }, completion: nil)
+    }
     
-    func showRow(_ row: UIStackView) {
-        if row.isHidden {
-            UIView.transition(with: gridRow1, duration: 0.5, options: .transitionCrossDissolve, animations: {
-                row.isHidden = false
-            }, completion: nil)
+    // hides the button image
+    func hideButton(_ button: UIButton) {
+        UIView.transition(with: button, duration: 0.4, options: .transitionCrossDissolve, animations: {
+            button.isHidden = true
+        }, completion: nil)
+    }
+    
+    @objc func dragGridView(_ sender: UIPanGestureRecognizer){
+        switch sender.state {
+        case .began, .changed : // suis le geste
+            transformGridView(gesture: sender)
+            /*
+        case .ended, .cancelled : // répond à la question
+            answerQuestion()
+ */
+        default:
+            break
         }
     }
     
-    func hideRow(_ row: UIStackView) {
-        if !row.isHidden {
-            UIView.transition(with: gridRow1, duration: 0.5, options: .transitionCrossDissolve, animations: {
-                row.isHidden = true
-            }, completion: nil)
-        }
+    private func transformGridView(gesture: UIPanGestureRecognizer){
+        let translation = gesture.translation(in: gridView)
+        gridView.transform = CGAffineTransform(translationX: 0, y: translation.y)
     }
 }
 
