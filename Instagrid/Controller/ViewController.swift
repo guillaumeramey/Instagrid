25c88d978ce516f1d20cf3083de7ed24dcaf2c6b
Guillaume Ramey <guillaume.ramey@gmail.com>
2018-10-30 13:39:29 +0200

    added animations

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -13,43 +13,51 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var gridRow1: UIStackView!
     @IBOutlet weak var gridRow2: UIStackView!
     @IBOutlet weak var gridRow3: UIStackView!
+    @IBOutlet weak var buttonLayout1: UIButton!
+    @IBOutlet weak var buttonLayout2: UIButton!
+    @IBOutlet weak var buttonLayout3: UIButton!
+    @IBOutlet weak var buttonImage1: UIButton!
+    @IBOutlet weak var buttonImage2: UIButton!
+    @IBOutlet weak var buttonImage3: UIButton!
+    @IBOutlet weak var buttonImage4: UIButton!
+    @IBOutlet weak var buttonImage5: UIButton!
+
+    @IBAction func tapButtonLayout1(_ sender: Any) {
+        setLayout(1)
+    }
+    @IBAction func tapButtonLayout2(_ sender: Any) {
+        setLayout(2)
+    }
+    @IBAction func tapButtonLayout3(_ sender: Any) {
+        setLayout(3)
+    }
+    @IBAction func tapButtonImage1(_ sender: UIButton) {
+        buttonSender = buttonImage1
+        self.present(imagePicker, animated: true)
+    }
+    @IBAction func tapButtonImage2(_ sender: UIButton) {
+        buttonSender = buttonImage2
+        self.present(imagePicker, animated: true)
+    }
+    @IBAction func tapButtonImage3(_ sender: UIButton) {
+        buttonSender = buttonImage3
+        self.present(imagePicker, animated: true)
+    }
+    @IBAction func tapButtonImage4(_ sender: UIButton) {
+        buttonSender = buttonImage4
+        self.present(imagePicker, animated: true)
+    }
+    @IBAction func tapButtonImage5(_ sender: UIButton) {
+        buttonSender = buttonImage5
+        self.present(imagePicker, animated: true)
+    }
     
     var imagePicker = UIImagePickerController()
-    var imagePicked = 0
-    
-    @IBOutlet weak var buttonImage1: UIButton!
-    @IBAction func tapButtonImage1(_ sender: UIButton) {
-        imagePicked = sender.tag
-        self.present(imagePicker, animated: true)
-    }
-    
-    @IBOutlet weak var buttonImage2: UIButton!
-    @IBAction func tapButtonImage2(_ sender: UIButton) {
-        imagePicked = sender.tag
-        self.present(imagePicker, animated: true)
-    }
-    
-    @IBOutlet weak var buttonImage3: UIButton!
-    @IBAction func tapButtonImage3(_ sender: UIButton) {
-        imagePicked = sender.tag
-        self.present(imagePicker, animated: true)
-    }
-    
-    @IBOutlet weak var buttonImage4: UIButton!
-    @IBAction func tapButtonImage4(_ sender: UIButton) {
-        imagePicked = sender.tag
-        self.present(imagePicker, animated: true)
-    }
-    
-    @IBOutlet weak var buttonImage5: UIButton!
-    @IBAction func tapButtonImage5(_ sender: UIButton) {
-        imagePicked = sender.tag
-        self.present(imagePicker, animated: true)
-    }
+    var buttonSender: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        layout = .layout2
+        setLayout(2)
         imagePicker.delegate = self
         imagePicker.sourceType = .savedPhotosAlbum
         imagePicker.allowsEditing = false
@@ -57,22 +65,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-        
-        switch imagePicked {
-        case 1:
-            buttonImage1.setImage(pickedImage, for: .normal)
-        case 2:
-            buttonImage2.setImage(pickedImage, for: .normal)
-        case 3:
-            buttonImage3.setImage(pickedImage, for: .normal)
-        case 4:
-            buttonImage4.setImage(pickedImage, for: .normal)
-        case 5:
-            buttonImage5.setImage(pickedImage, for: .normal)
-        default:
-            break
-        }
-        
+        buttonSender.setImage(pickedImage, for: .normal)
+        buttonSender.imageView?.contentMode = UIView.ContentMode.scaleAspectFill
+        buttonSender.backgroundColor = #colorLiteral(red: 0.06274509804, green: 0.4, blue: 0.5960784314, alpha: 1)
         dismiss(animated: true)
     }
     
@@ -80,54 +75,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true)
     }
     
-    enum Layout {
-        case layout1, layout2, layout3
-    }
-    
-    var layout: Layout = .layout2 {
-        didSet {
-            setLayout(layout)
+    func setLayout(_ layout: Int) {
+        switch layout {
+        case 1:
+            selectButtonLayout(buttonLayout1)
+            showRow(gridRow2)
+            showRow(gridRow3)
+            hideRow(gridRow1)
+        case 2:
+            selectButtonLayout(buttonLayout2)
+            showRow(gridRow1)
+            showRow(gridRow2)
+            hideRow(gridRow3)
+        case 3:
+            selectButtonLayout(buttonLayout3)
+            showRow(gridRow1)
+            showRow(gridRow3)
+            hideRow(gridRow2)
+        default:
+            break
         }
     }
     
-    @IBOutlet weak var buttonLayout1: UIButton!
-    @IBAction func tapButtonLayout1(_ sender: Any) {
-        layout = .layout1
+    func selectButtonLayout(_ buttonLayout: UIButton) {
+        buttonLayout1.setImage(UIImage(named: ""), for: .normal)
+        buttonLayout2.setImage(UIImage(named: ""), for: .normal)
+        buttonLayout3.setImage(UIImage(named: ""), for: .normal)
+        buttonLayout.setImage(UIImage(named: "Button Selected.png"), for: .normal)
     }
     
-    @IBOutlet weak var buttonLayout2: UIButton!
-    @IBAction func tapButtonLayout2(_ sender: Any) {
-        layout = .layout2
+    func showRow(_ row: UIStackView) {
+        if row.isHidden {
+            UIView.transition(with: gridRow1, duration: 0.5, options: .transitionCrossDissolve, animations: {
+                row.isHidden = false
+            }, completion: nil)
+        }
     }
     
-    @IBOutlet weak var buttonLayout3: UIButton!
-    @IBAction func tapButtonLayout3(_ sender: Any) {
-        layout = .layout3
-    }
-    
-    func setLayout(_ layout: Layout) {
-        switch layout {
-        case .layout1:
-            buttonLayout1.setImage(UIImage(named: "Button Selected.png"), for: .normal)
-            buttonLayout2.setImage(UIImage(named: ""), for: .normal)
-            buttonLayout3.setImage(UIImage(named: ""), for: .normal)
-            gridRow1.isHidden = true
-            gridRow2.isHidden = false
-            gridRow3.isHidden = false
-        case .layout2:
-            buttonLayout1.setImage(UIImage(named: ""), for: .normal)
-            buttonLayout2.setImage(UIImage(named: "Button Selected.png"), for: .normal)
-            buttonLayout3.setImage(UIImage(named: ""), for: .normal)
-            gridRow1.isHidden = false
-            gridRow2.isHidden = false
-            gridRow3.isHidden = true
-        case .layout3:
-            buttonLayout1.setImage(UIImage(named: ""), for: .normal)
-            buttonLayout2.setImage(UIImage(named: ""), for: .normal)
-            buttonLayout3.setImage(UIImage(named: "Button Selected.png"), for: .normal)
-            gridRow1.isHidden = false
-            gridRow2.isHidden = true
-            gridRow3.isHidden = false
+    func hideRow(_ row: UIStackView) {
+        if !row.isHidden {
+            UIView.transition(with: gridRow1, duration: 0.5, options: .transitionCrossDissolve, animations: {
+                row.isHidden = true
+            }, completion: nil)
         }
     }
 }
